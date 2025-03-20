a1b0ed8b59caeb63151f9641c571851f7275956b
ryaskye <ryaskye@gmail.com>
2025-03-20 19:27:06 -0400

    2 commit

---
+++ LSP_midterm/src/org/howard/edu/lsp/midterm/question2/Book.java
@@ -0,0 +1,65 @@
+package org.howard.edu.lsp.midterm.question2;
+
+public class Book {
+    private String title;
+    private String author;
+    private String ISBN;
+    private int yearPublished;
+
+    // Constructor
+    public Book(String title, String author, String ISBN, int yearPublished) {
+        this.title = title;
+        this.author = author;
+        this.ISBN = ISBN;
+        this.yearPublished = yearPublished;
+    }
+
+    
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public int getYearPublished() {
+        return yearPublished;
+    }
+
+    // Setters
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public void setYearPublished(int yearPublished) {
+        this.yearPublished = yearPublished;
+    }
+
+    // Override equals method
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Book book = (Book) obj;
+        return ISBN.equals(book.ISBN) && author.equals(book.author);
+    }
+
+    // Override toString method
+    @Override
+    public String toString() {
+        return "Title: " + title + ", Author: " + author + ", ISBN: " + ISBN + ", Year Published: " + yearPublished;
+    }
+}
